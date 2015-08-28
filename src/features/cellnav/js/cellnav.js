fb99f52a4b250e7786c8cac4c66ca9a69d9a14da
lvostinar@servoy.com <lvostinar@servoy.com>
2015-08-28 14:12:10 +0300

    fix for https://support.servoy.com/browse/SVY-8687, enter should not be used for navigation

--- src/features/cellnav/js/cellnav.js
+++ src/features/cellnav/js/cellnav.js
@@ -438,8 +438,7 @@
             return uiGridCellNavConstants.direction.RIGHT;
           }
 
-          if (evt.keyCode === uiGridConstants.keymap.UP ||
-            (evt.keyCode === uiGridConstants.keymap.ENTER && evt.shiftKey) ) {
+          if (evt.keyCode === uiGridConstants.keymap.UP) {
             return uiGridCellNavConstants.direction.UP;
           }
 
@@ -447,8 +446,7 @@
             return uiGridCellNavConstants.direction.PG_UP;
           }
 
-          if (evt.keyCode === uiGridConstants.keymap.DOWN ||
-            evt.keyCode === uiGridConstants.keymap.ENTER) {
+          if (evt.keyCode === uiGridConstants.keymap.DOWN) {
             return uiGridCellNavConstants.direction.DOWN;
           }
 
